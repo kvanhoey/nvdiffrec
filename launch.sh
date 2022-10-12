78598fe598bbae8e44e734240dafc33bc47d6dd4
Kenneth Vanhoey <kennethv@unity3d.com>
2022-10-12 18:16:51 +0200

    Add launcher file

---
+++ launch.sh
@@ -0,0 +1,4 @@
+python train.py --config configs/our_statue_new_vert_resol4.json
+python train.py --config configs/our_statue_new_vert_resol8.json
+python train.py --config configs/our_statue_new_hor_resol4.json
+python train.py --config configs/our_statue_new_hor_resol8.json
