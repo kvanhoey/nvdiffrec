e1ffb77957f035b31c34fbd006a895ca24974a64
Kenneth Vanhoey <kennethv@research.kvanhoey.eu>
2022-10-18 17:48:04 +0200

    .

--- launch.sh
+++ launch.sh
@@ -1,4 +1,13 @@
-python train.py --config configs/our_statue_new_vert_resol4.json
-python train.py --config configs/our_statue_new_vert_resol8.json
-python train.py --config configs/our_statue_new_hor_resol4.json
-python train.py --config configs/our_statue_new_hor_resol8.json
+#python train.py --config configs/our_statue_new_vert_resol4.json
+#python train.py --config configs/our_statue_new_vert_resol8.json
+#python train.py --config configs/our_statue_new_hor_resol4.json
+#python train.py --config configs/our_statue_new_hor_resol8.json
+#python train.py --config configs/our_statue_new_vert_resol4diffuse.json
+#python train.py --config configs/our_statue_new_vert_resol4lockgeom.json
+#python train.py --config configs/our_statue_new_vert_resol4LRlow.json
+python train.py --config configs/our_statue_new_vert_resol4lr0.json
+python train.py --config configs/our_statue_new_vert_resol4lr1.json
+python train.py --config configs/our_statue_new_vert_resol4lr2.json
+python train.py --config configs/our_statue_new_vert_resol4lr1-256.json
+python train.py --config configs/our_statue_new_vert_resol4lr1-512.json
+
