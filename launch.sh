f2578ae487b35907af27694862b4a0a0a9423c96
Kenneth Vanhoey <kenneth@research.kvanhoey.eu>
2022-10-19 12:26:59 +0200

    llauncer

--- launch.sh
+++ launch.sh
@@ -5,9 +5,14 @@
 #python train.py --config configs/our_statue_new_vert_resol4diffuse.json
 #python train.py --config configs/our_statue_new_vert_resol4lockgeom.json
 #python train.py --config configs/our_statue_new_vert_resol4LRlow.json
-python train.py --config configs/our_statue_new_vert_resol4lr0.json
-python train.py --config configs/our_statue_new_vert_resol4lr1.json
-python train.py --config configs/our_statue_new_vert_resol4lr2.json
-python train.py --config configs/our_statue_new_vert_resol4lr1-256.json
-python train.py --config configs/our_statue_new_vert_resol4lr1-512.json
+#python train.py --config configs/our_statue_new_vert_resol4lr0.json
+#python train.py --config configs/our_statue_new_vert_resol4lr1.json
+#python train.py --config configs/our_statue_new_vert_resol4lr2.json
+#python train.py --config configs/our_statue_new_vert_resol4lr1-256.json
+#python train.py --config configs/our_statue_new_vert_resol4lr1-512.json
+python train.py --config configs/our_statue_new_vert_resol4lr2-regul0.json
+python train.py --config configs/our_statue_new_vert_resol4lr2-regul1.json
+python train.py --config configs/our_statue_new_vert_resol4lr2-regul2.json
+python train.py --config configs/our_statue_new_vert_resol4lr2-regul3.json
+
 
