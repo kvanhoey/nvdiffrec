7bb418316f746954a66061960b90cac862d24b70
kvanhoey <kennethv@unity3d.com>
2022-11-17 17:49:44 +0100

    Add configs

--- launch.sh
+++ launch.sh
@@ -22,26 +22,30 @@
 python train.py --config configs/our_statue_new_vert_resol4lr0.json
 python train.py --config configs/our_statue_new_vert_resol4lr1.json
 python train.py --config configs/our_statue_new_vert_resol4lr2.json
-
+ 
 python train.py --config configs/our_statue_new_vert_resol4lr2-regul0.json
 python train.py --config configs/our_statue_new_vert_resol4lr2-regul1.json
 python train.py --config configs/our_statue_new_vert_resol4lr2-regul2.json
 python train.py --config configs/our_statue_new_vert_resol4lr2-regul3.json
 
-python train.py --config configs/our_statue_vert_stage2lr0.json
-python train.py --config configs/our_statue_vert_stage2lr1.json
-python train.py --config configs/our_statue_vert_stage2lr2.json
+# python train.py --config configs/our_statue_vert_stage2lr0.json
+# python train.py --config configs/our_statue_vert_stage2lr1.json
+# python train.py --config configs/our_statue_vert_stage2lr2.json
+python train.py --config configs/our_statue_vert_stage2lr3.json
+python train.py --config configs/our_statue_vert_stage2lr4.json
 
-python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr0.json
-python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr1.json
-python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom.json
-python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr3.json
+python train.py --config configs/our_statue_vert_resol4-uvpenelope_vargeom-lr3.json
+python train.py --config configs/our_statue_vert_resol4-uvpenelope_vargeom-lr4.json
 
-python train.py --config configs/our_statue_vert_resol4-uvcyril_fixgeom.json
-python train.py --config configs/our_statue_vert_resol4-uvcyril_fixgeomnorm.json
 
-python train.py --config configs/our_statue_vert_resol4-uvpenelope_vargeom.json
-python train.py --config configs/our_statue_vert_resol4-uvpenelope_fixgeom.json
-python train.py --config configs/our_statue_vert_resol4-uvpenelope_fixgeomnorm.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr0.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr1.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_vargeom-lr3.json
 
-python train.py --config configs/our_statue_vert_resol4-256.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_fixgeom.json
+# python train.py --config configs/our_statue_vert_resol4-uvcyril_fixgeomnorm.json
+
+# python train.py --config configs/our_statue_vert_resol4-uvpenelope_vargeom.json
+# python train.py --config configs/our_statue_vert_resol4-uvpenelope_fixgeom.json
+# python train.py --config configs/our_statue_vert_resol4-uvpenelope_fixgeomnorm.json
